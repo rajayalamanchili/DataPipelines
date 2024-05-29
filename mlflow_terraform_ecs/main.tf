9c6da804f142628d51ab732ba94ae3940ff0a962
Raja PC Yalamanchili <raja.yalamanchili2@gmail.com>
2024-05-29 03:34:52 +0000

    feat: updated load balancer to access mlflow container

--- mlflow_terraform_ecs/main.tf
+++ mlflow_terraform_ecs/main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "lb_sg_egress" {
   type                     = "egress"
   from_port                = 0
   to_port                  = 0
-  protocol                 = "tcp"
+  protocol                 = "-1"
   cidr_blocks              = ["0.0.0.0/0"]
   security_group_id        = aws_security_group.lb_sg.id
 }
